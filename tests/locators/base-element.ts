a17cc78d33326d9629d67b303a4f0da825f28740
g.bagratian <g.bagratian@sbereducation.ru>
2025-01-26 16:00:29 +0300

    pw added

---
+++ tests/locators/base-element.ts
@@ -0,0 +1,41 @@
+import { Locator, test } from "playwright/test";
+
+export class BaseElement {
+    protected readonly locator: Locator;
+    protected readonly name: string;
+
+    constructor(locator: Locator, name: string) {
+        this.locator = locator;
+        this.name = `${name}`;
+    }
+
+    get getLocator(): Locator {
+        return this.locator;
+    }
+
+    get getName(): string {
+        return this.name;
+    }
+
+    async getText(): Promise<string> {
+        return this.locator.innerText();
+    }
+
+    async hover(): Promise<void> {
+        await test.step(
+            `Навести курсор на блок [${this.name}]`,
+            async () => {
+                await this.locator.hover();
+            },
+            { box: true },
+        );
+    }
+
+    async setFocus(): Promise<void> {
+        return this.locator.click();
+    }
+
+    async scrollToElementIfNeeded(): Promise<void> {
+        return this.locator.scrollIntoViewIfNeeded();
+    }
+}
